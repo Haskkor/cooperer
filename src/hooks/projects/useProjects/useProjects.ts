68d7030f971d605f06972ab458cb0391c8d8bbb3
Jeremy Farnault <haskkor@gmail.com>
2021-02-10 09:15:37 +1300

    feat: trying first queries

---
+++ src/hooks/projects/useProjects/useProjects.ts
@@ -0,0 +1,26 @@
+import { API, graphqlOperation } from 'aws-amplify';
+import { useQuery } from 'react-query';
+
+import { listProjects } from '../../../graphql/queries';
+
+const useProjects = () => {
+
+  const { data, isLoading, refetch, error } = useQuery(
+    ['post'],
+    async () => {
+      const result: any = await API.graphql(graphqlOperation(listProjects));
+      return result.data as any; // fixme any
+    }
+  );
+
+  const projects = data.listProjects.items;
+
+  return {
+    error,
+    isLoading,
+    projects,
+    refetch
+  }
+};
+
+export default useProjects;
